b92090a8bcd8c29c5faadafc18beb8d2f278cbfe
Aboda72 <53348107+Aboda72@users.noreply.github.com>
2020-09-10 08:14:29 +0200

    documenting the solution

--- Restoring Password.c
+++ Restoring Password.c
@@ -3,19 +3,22 @@
 int main() {
     char encrypted [8][11] , sybmol_table [10] [11] , reversed [9] ;
     int i = 0 , j = 0 ;
- 
+     /* git the whole password */
     for(i = 0 ; i < 8 ; i++)
     {
         for(j = 0 ; j < 10 ; j++)
         {
             scanf(" %c",&encrypted [i][j]) ;
         }
+        /* segment the whole password */
         encrypted [i][j] = '\0' ;
     }
+    /* git keys */
     for(int i = 0 ; i < 10 ; i++)
     {
         scanf("%s",sybmol_table[i]);
     }
+    /* search the real value for every segment */
     for(i = 0 ; i < 8 ; i++)
     {
         if(!strcmp(sybmol_table[0] ,encrypted [i] ))
