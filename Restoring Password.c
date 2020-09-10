6505f060f51119d8728668fe0a6e9442f1053d73
7aposha <abdohepish13@gmail.com>
2020-09-10 08:09:22 +0200

    Solution of : 94A---Restoring-Password.c in C

---
+++ Restoring Password.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char encrypted [8][11] , sybmol_table [10] [11] , reversed [9] ;
+    int i = 0 , j = 0 ;
+ 
+    for(i = 0 ; i < 8 ; i++)
+    {
+        for(j = 0 ; j < 10 ; j++)
+        {
+            scanf(" %c",&encrypted [i][j]) ;
+        }
+        encrypted [i][j] = '\0' ;
+    }
+    for(int i = 0 ; i < 10 ; i++)
+    {
+        scanf("%s",sybmol_table[i]);
+    }
+    for(i = 0 ; i < 8 ; i++)
+    {
+        if(!strcmp(sybmol_table[0] ,encrypted [i] ))
+        {
+          reversed [i] = '0' ;
+        } else if (!strcmp(sybmol_table[1] ,encrypted [i] ))
+        {
+            reversed [i] = '1' ;
+        } else if (!strcmp(sybmol_table[2] ,encrypted [i] ))
+        {
+            reversed [i] = '2' ;
+        } else if (!strcmp(sybmol_table[3] ,encrypted [i] ))
+        {
+            reversed [i] = '3' ;
+        } else if (!strcmp(sybmol_table[4] ,encrypted [i] ))
+        {
+            reversed [i] = '4' ;
+        } else if(!strcmp(sybmol_table[5] ,encrypted [i] ))
+        {
+            reversed [i] = '5' ;
+        } else if(!strcmp(sybmol_table[6] ,encrypted [i] ))
+        {
+            reversed [i] = '6' ;
+        } else if(!strcmp(sybmol_table[7] ,encrypted [i] ))
+        {
+            reversed [i] = '7' ;
+        } else if(!strcmp(sybmol_table[8] ,encrypted [i] ))
+        {
+            reversed [i] = '8' ;
+        } else if(!strcmp(sybmol_table[9] ,encrypted [i] ))
+        {
+            reversed [i] = '9' ;
+        }
+        else
+        {
+            /* should not be here */
+        }
+    }
+    reversed [i] = '\0' ;
+    printf("%s",reversed) ;
+    return 0;
+}
